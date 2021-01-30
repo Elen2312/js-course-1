2ee04c7664a8a844e179f95753aefe04d2d9bef4
Elena <elen_job@bk.ru>
2021-01-31 00:16:02 +0300

    game bulls and cows 1

--- bulls_and_cows.js
+++ bulls_and_cows.js
@@ -29,40 +29,45 @@ function start() {
 function check(numUser) {
     let bulls = 0;
     let cows = 0;
-    let move = Number(document.getElementById('move').innerHTML);
+    let move = Number(document.getElementById('move').textContent);
 
     for (let i = 0; i < 4; i++) {
-        if (numUser[i] == numPC[i]) {
+        if (numUser[i] === numPC[i]) {
             bulls++;
-        }else if (numPC.indexOf(numUser[i])>=0) {
+        } else if (numPC.indexOf(numUser[i]) >= 0) {
             cows++;
         }
     }
-    console.log(numPC + ' b: '+ bulls + ' c: ' + cows);
+    console.log(numPC + ' b: ' + bulls + ' c: ' + cows);
 
     move--;
-    document.getElementById('move').innerHTML = move;
+    document.getElementById('move').textContent = move;
 
-    // ВОТ ДО ЭТОГО МОМЕНТА ВСЕ РАБОТАЕТ
+    // НЕ РАБОТАЕТ ФУНКЦИЯ СКЛОНЕНИЯ СЛОВА
 
-    if ((move == 0) || (bulls == 4)) {
+    function num_word() {
+       let move = Number(document.getElementById('move').value);
+       document.getElementById('word').textContent = (move === 1) ? 'ход' : (move % 10 > 1 && move % 10 < 5) ? 'хода' : 'ходов';
+    }
+
+    if ((move === 0) || (bulls === 4)) {
         let status = ' проиграли';
-        if (bulls == 4) {
+        if (bulls === 4) {
             let status = ' выиграли';
         }
-       gameOver();
+       gameOver(numPC, status);
     }
-    result()
+    result(numUser, bulls, cows);
 }
 
 function result(numUser, bulls, cows) {
     let mes = document.getElementById('message');
     let newLine = document.createElement('p');
-    newLine.innerHTML = numUser + ' быки: ' + bulls + '; коровы: ' + cows;
+    newLine.textContent = numUser + ' быки: ' + bulls + '; коровы: ' + cows;
     mes.appendChild(newLine);
 }
 
-function gameOver(numPC, move, status) {
+function gameOver(numPC, status) {
     alert('Вы ' + status + '\nЗагаданное число: ' + numPC);
     location.reload();
 }
